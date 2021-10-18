beaaeea08392410d5455b84c9584f0138a8297ca
Jun Wu <junlin.wu@sjsu.edu>
2021-10-18 16:44:47 -0700

    Added BankAccountTester and updated BankAccount

---
+++ UnitTests/BankAccountTester.java
@@ -0,0 +1,80 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BankAccountTester
+{
+    private BankAccount account1;
+
+    @BeforeEach
+    public void setUp()
+    {
+        account1 = new BankAccount("Jun", "Wu", 1000);
+    }
+
+    @Test
+    public void getFirstNameTest()
+    {
+        assertEquals("Jun", account1.getFirstName(), "Should be Jun");
+    }
+
+    @Test
+    public void getLastNameTest()
+    {
+        assertEquals("Wu", account1.getLastName(), "Should be Wu");
+    }
+
+    @Test
+    public void getBalanceTest()
+    {
+        assertEquals(1000, account1.getBalance(), "Should be 1000");
+    }
+
+    @Test
+    public void updateBalanceDepositTest()
+    {
+        assertEquals(1500, account1.updateBalance(500), "Should be 1500");
+    }
+
+    @Test
+    public void updateBalanceWithdrawalTest()
+    {
+        assertEquals(500, account1.updateBalance(-500), "Should be 500");
+    }
+
+    @Test
+    public void smallerObjFirstCompareToTest()
+    {
+        BankAccount account2 = new BankAccount("Shivam", "Amin", 1000);
+        assertEquals(-1, account1.compareTo(account2), "Should be -1");
+    }
+
+    @Test
+    public void LargerObjFirstCompareToTest()
+    {
+        BankAccount account2 = new BankAccount("Gargi", "Tawde", 1000);
+        assertEquals(1, account1.compareTo(account2), "Should be 1");
+    }
+
+    @Test
+    public void equalObjCompareToTest()
+    {
+        BankAccount account2 = new BankAccount("Jun", "Wu", 1000);
+        assertEquals(0, account1.compareTo(account2), "Should be 0");
+    }
+
+    @Test
+    public void validEqualsTest()
+    {
+        BankAccount account2 = new BankAccount("Jun", "Wu", 1000);
+        assertTrue(account1.equals(account2), "Should be true");
+    }
+
+    @Test
+    public void invalidEqualsTest()
+    {
+        BankAccount account2 = new BankAccount("Chint", "Patel", 1500);
+        assertFalse(account1.equals(account2), "Should be false");
+    }
+}
--- src/BankAccount.java
+++ src/BankAccount.java
@@ -24,25 +24,57 @@ public class BankAccount implements Comparable<BankAccount>
         return balance;
     }
 
-    public void updateBalance(double amount)
+    public double updateBalance(double amount)
     {
         balance += amount;
+        return this.balance;
     }
 
     @Override
     public int compareTo(BankAccount o) {
-        if(!this.firstName.equals(o.firstName)){
-            return this.firstName.compareTo(o.firstName);
-        }else if(!this.lastName.equals(o.lastName)){
-            return this.lastName.compareTo(o.lastName);
-        }else{
-            return (int)Math.signum(o.balance - this.balance);
+        if(this.firstName.compareTo(o.firstName) < 0)
+        {
+            return -1;
+        }
+        else if(this.firstName.compareTo(o.firstName) > 0)
+        {
+            return 1;
+        }
+        else
+        {
+            if(this.lastName.compareTo(o.lastName) < 0)
+            {
+                return -1;
+            }
+            else if(this.lastName.compareTo(o.lastName) > 0)
+            {
+                return 1;
+            }
+            else
+                {
+                    if(this.balance > o.balance)
+                    {
+                        return -1;
+                    }
+                    else if(this.balance < o.balance)
+                    {
+                        return 1;
+                    }
+                    else
+                    {
+                        return 0;
+                    }
+                }
         }
     }
     @Override
-    public boolean equals(Object o) {
-        BankAccount u = (BankAccount)o;
-        return this.compareTo(u) == 0;
+    public boolean equals(Object o)
+    {
+        if(o instanceof BankAccount) {
+            BankAccount u = (BankAccount) o;
+            return this.compareTo(u) == 0;
+        }
+        return false;
     }
 }
 
