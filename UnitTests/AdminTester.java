81f3d173ac1bdad8dad62b3572b35a6054f34443
Chint Patel <patelchint2002@gmail.com>
2021-11-01 21:11:59 -0700

    Created UserTester Class and add JavaDoc Comments to User Class

--- UnitTests/AdminTester.java
+++ UnitTests/AdminTester.java
@@ -1,3 +1,5 @@
+package UnitTests;
+
 import org.junit.*;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
--- UnitTests/TransactionTester.java
+++ UnitTests/TransactionTester.java
@@ -1,3 +1,5 @@
+package UnitTests;
+
 import org.junit.*;
 import static org.junit.jupiter.api.Assertions.*;
 
