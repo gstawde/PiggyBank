066a6ef84a191984b26a9e79bca8ad4a170ec6a7
Chint Patel <patelchint2002@gmail.com>
2021-11-10 19:37:15 -0800

    commit RequestTransferView class setup

--- UnitTests/BankAccountTester.java
+++ UnitTests/BankAccountTester.java
@@ -1,6 +1,7 @@
 import Model.BankAccount;
+import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
