829947de2f44016d227fdd99525e208b6face957
Jun Wu <junlin.wu@sjsu.edu>
2021-11-27 18:41:25 -0800

    update SettingView and added queue and messages to setting control

---
+++ src/Controller/UpdatePasswordMessage.java
@@ -0,0 +1,16 @@
+package Controller;
+
+public class UpdatePasswordMessage implements Message
+{
+    private String password;
+
+    public UpdatePasswordMessage(String password)
+    {
+        this.password = password;
+    }
+
+    public String getPassword()
+    {
+        return this.password;
+    }
+}
